0ca3ffde1bd325cb3af8d37be2fdd04280c64c76
winnyace <winnyace82@gmail.com>
2023-09-07 17:48:49 +0300

    I've meme'd

---
+++ Extra_LeftPadMeme/Program.cs
@@ -0,0 +1,13 @@
+﻿static string LeftPad(string input, int num, char ch)
+{
+    string output = "";
+    while (num != 0)
+    {
+        output += ch.ToString();
+        num--;
+    }
+    output += input;
+    return output;
+}
+
+Console.WriteLine(LeftPad("Test", 5, ' '));
