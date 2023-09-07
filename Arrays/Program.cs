c3911cfbc2ea7ec521997d7590dbd2189445d7e9
winnyace <winnyace82@gmail.com>
2023-09-07 11:23:44 +0300

    more shit done. fuck vscode btw.

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] things = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (var i in things)
+{
+    if (i.StartsWith("B"))
+        Console.WriteLine(i);
+}
+
+// Below are some important things to keep in mind.
+// int[] v = new int[3];
+// for (int i = 0; i < 3; i++)
+// {
+//     v[i] = Convert.ToInt32(Console.ReadLine());
+// }
+//
+// foreach (int n in v)
+// {
+//     Console.WriteLine(n);
+// }
---
+++ ReadableCode/Program.cs
@@ -0,0 +1,14 @@
+﻿string str = "The quick brown fox jumps over the lazy dog.";
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+int x = 0;
+foreach (char i in charMessage) 
+{ 
+    if (i == 'o') 
+    {
+        x++; 
+    } 
+}
+string newMessage = new String(charMessage);
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {x} times.");
